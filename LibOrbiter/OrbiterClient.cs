8281f82da86950f5dd0f2b0bc7c6423a6142d721
Urias <GodofDrakes@gmail.com>
2022-01-06 12:26:35 -0800

    Initial commit

---
+++ LibOrbiter/OrbiterClient.cs
@@ -0,0 +1,16 @@
+﻿
+using RestSharp;
+
+namespace LibOrbiter
+{
+    public class OrbiterClient
+    {
+        private readonly RestClient restClient;
+
+        public OrbiterClient(string? serviceId = default)
+        {
+            restClient = new($"http://census.daybreakgames.com/s:{serviceId ?? "example"}/json");
+            restClient.UseSerializer<RestSharp.Serializers.NewtonsoftJson.JsonNetSerializer>();
+        }
+    }
+}
